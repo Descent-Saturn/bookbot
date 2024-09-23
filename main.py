02ddf42a24f6d761262c031826cfe87f370afbc4
Descent-Saturn <alfacharlie1@hotmail.com>
2024-09-23 19:59:42 -0400

    B: Added word count to the main.py file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
 	with open("books/frankenstein.txt") as f:
 		file_contents = f.read()
-	print (file_contents)
-main ()
+	word_count(file_contents)
+def word_count (file_contents):
+    total_words = file_contents.split()
+    print (len(total_words))
+main()
